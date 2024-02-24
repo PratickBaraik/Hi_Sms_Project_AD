bd26cf4f0f27a72d0ce64f2912842e5acff01d0a
PratickB111 <animeNerd.int@protonmail.com>
2024-02-24 12:11:11 +0530

    registration and authentication done!

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,6 +43,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.google.firebase:firebase-auth-ktx:22.3.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -53,4 +54,7 @@ dependencies {
 
     // adding firebase analytics dependency
     implementation("com.google.firebase:firebase-analytics")
+
+    // adding authentication dependency for registration
+    implementation("com.google.firebase:firebase-auth")
 }
--- app/src/main/java/com/example/hisms/RegisterActivity.kt
+++ app/src/main/java/com/example/hisms/RegisterActivity.kt
@@ -5,7 +5,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils
 import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 
 class RegisterActivity : AppCompatActivity() {
 
@@ -15,37 +17,40 @@ class RegisterActivity : AppCompatActivity() {
     lateinit var rePass: String
     lateinit var email: String
     lateinit var phNumber: String
+    lateinit var mAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+        mAuth = FirebaseAuth.getInstance()
 
         // action listener to go to main activity
         val mainButton = findViewById<Button>(R.id.mainActivity)
 
         mainButton.setOnClickListener{
 
-            name = (R.id.fullNameText).toString()
-            password = (R.id.passwordText).toString()
-            rePass = (R.id.rePasswordText).toString()
-            email = (R.id.emailText).toString()
-            phNumber = R.id.phoneText.toString()
+            name = findViewById<EditText>(R.id.fullNameText).text.toString()
+            password = findViewById<EditText>(R.id.passwordText).text.toString()
+            rePass = findViewById<EditText>(R.id.rePasswordText).text.toString()
+            email = findViewById<EditText>(R.id.emailText).text.toString()
+            phNumber = findViewById<EditText>(R.id.phoneText).text.toString()
 
             if(TextUtils.isEmpty(name) || TextUtils.isEmpty(password) || TextUtils.isEmpty(rePass) || TextUtils.isEmpty(email) || TextUtils.isEmpty(phNumber)) {
                 Toast.makeText(this, "Please fill all the details", Toast.LENGTH_SHORT).show()
             } else {
-                //val myIntent = Intent(this, MainActivity::class.java)
-                //startActivity(myIntent)
-                //Toast.makeText(MainActivity::class.java, "Registration Successful!", Toast.LENGTH_SHORT).show()
+                createAccount(email, password)
             }
 
         }
-
-        // creating a createAccount function
-      //  createAccount(email, password)
     }
 
-    //fun createAccount(email: String, password: String) {
-
-    //}
+    fun createAccount(email: String, password: String) {
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener {
+            if(it.isSuccessful) {
+                Toast.makeText(this, "Registration successful", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "${it.exception?.localizedMessage.toString()}", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
 }
